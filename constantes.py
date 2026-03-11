15b9c366d2e9c79077504ce988a6be7ee7816856
arthurfalzetta <arthur.falzetta@gmail.com>
2026-03-11 18:23:48 -0300

    feat: adiciona configuração via .env para mapa, simulação e rede neural

--- constantes.py
+++ constantes.py
@@ -8,5 +8,8 @@ LARGURA_MAPA = int(os.getenv("LARGURA_MAPA", 900))
 
 NUM_AGENTES = int(os.getenv("NUM_AGENTES", 20))
 FPS = int(os.getenv("FPS", 60))
-
 VELOCIDADE_AGENTE = float(os.getenv("VELOCIDADE_AGENTE", 200))
+
+INPUT_SIZE = int(os.getenv("INPUT_SIZE", 3))
+HIDDEN_SIZE = int(os.getenv("HIDDEN_SIZE", 6))
+OUTPUT_SIZE = int(os.getenv("OUTPUT_SIZE", 2))
--- rede_neural.py
+++ rede_neural.py
@@ -1,8 +1,9 @@
+from constantes import INPUT_SIZE, HIDDEN_SIZE, OUTPUT_SIZE
 import numpy as np
 
 
 class RedeNeural:
-    def __init__(self, input_size=3, hidden_size=6, output_size=2):
+    def __init__(self, input_size=INPUT_SIZE, hidden_size=HIDDEN_SIZE, output_size=OUTPUT_SIZE):
         #pesos aleatorios
         self.w1 = np.random.randn(input_size, hidden_size)
         self.w2 = np.random.randn(hidden_size, output_size)
